7011c63364639bd1fac27394bb6a0fc6551c9460
JoelTapia99 <joelts159@gmail.com>
2020-08-04 20:57:32 -0500

    commit con tabla.js

---
+++ tabla.js
@@ -0,0 +1,9 @@
+// obtengo la variable 'datos' del archivo main.js
+
+document.getElementById('tabla').innerHTML = `<tr>
+<th scope="row">1</th>
+<td>ejemplo</td>
+<td>ejemplo</td>
+<td>ejemplo</td>
+<td>ejemplo</td>
+</tr>`;
